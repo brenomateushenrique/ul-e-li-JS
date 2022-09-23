71b49fc2ee16437da8c9e1b1caf846c31af214b9
brenomateushenrique <114258671+brenomateushenrique@users.noreply.github.com>
2022-09-23 16:32:10 -0300

    commit inicial
    atvd 02 - react

---
+++ App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+
+function App(){
+    return (
+    <div className='dp-menu'>
+
+        <ul>
+            <li> <a href='#'>Produtos</a>
+                <ul>
+                    <li><a href='#'>Camisas</a></li>
+                    <li><a href='#'>Calças</a></li>
+                    <li><a href='#'>Sapatos</a></li>
+                </ul>
+                <li><a href='#'>Cozinha</a>
+                <ul>
+                    <li><a href='#'>Fogão</a></li>
+                    <li><a href='#'>Geladeira</a></li>
+                    <li><a href='#'>Micro-Ondas</a></li>
+                </ul>
+                </li>
+            </li>
+        </ul>
+
+
+        
+    </div>
+
+    );
+
+}
+
+
+export default App;
